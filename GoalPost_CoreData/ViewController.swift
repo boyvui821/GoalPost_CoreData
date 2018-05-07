314d7c0056641842ac9f6e870c09242e4a517cd7
Nguyen Hieu Trung <ktpm11520669@gmail.com>
2018-05-07 11:25:04 +0700

    Initial Commit

---
+++ GoalPost_CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalPost_CoreData
+//
+//  Created by Nguyen Hieu Trung on 5/7/18.
+//  Copyright © 2018 NHTSOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
